190423d703679e5e3938c512c844e11eaf2a2c98
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-08-08 21:25:36 -0400

    Changed JWT token lifetime to 15min

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -319,6 +319,6 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/api/v1/logout$}]
     ]
-    jwt.expiration_time = 30.minutes.to_i # Access jwt token lifetime
+    jwt.expiration_time = 15.minutes.to_i # JWT Access token lifetime
   end
 end
