f2a1312b3f60856900f358bd7984dbf7f3482af7
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-08-09 13:52:50 -0400

    User edit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,6 @@ class Api::V1::UsersController < Api::V1::BaseController
   def show
     @user = User.find(params[:id])
     authorize @user
-    # render json: @user
     render json: UserSerializer.new(@user)
   end
 end
