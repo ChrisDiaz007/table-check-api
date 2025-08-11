fad95f927f3c06dea85a8d028fe9fba0819e8cee
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-08-11 19:49:16 -0400

    Minor edits

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,13 +1,13 @@
 class UserPolicy < ApplicationPolicy
   class Scope < ApplicationPolicy::Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
   end
 
   def show?
-    return true
-    # return user.admin? || record == user
+    # return true
+    return user.admin? || record == user
   end
 end
