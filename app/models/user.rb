699a67571aa69c3983ab02b2a0c65ed05d55faf0
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-08-29 10:50:59 -0400

    minor text

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,6 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_restaurants, through: :favorites, source: :restaurant
 
-
-
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
