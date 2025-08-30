0d4ca3486336af43a9d9a83f7a81b63a1dc0119b
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-08-29 20:00:22 -0400

    Updated params and added validations for user(first name last name)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,7 +29,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name)
+    params.require(:user).permit(:first_name, :last_name, :phone_number)
   end
 
 end
