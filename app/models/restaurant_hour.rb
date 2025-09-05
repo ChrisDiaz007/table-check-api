b9d420d15b53e3ea799ddb108879356e2500307d
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-09-05 09:35:16 -0400

    Added validations to Restaurant hours model, created restaurant hour serializer

--- app/models/restaurant_hour.rb
+++ app/models/restaurant_hour.rb
@@ -1,7 +1,8 @@
 class RestaurantHour < ApplicationRecord
   belongs_to :restaurant
 
-  validates :day_of_week, presence: true
   validates :opens_at, presence: true
   validates :closes_at, presence: true
+  validates :day_of_week, presence: true, inclusion: { in: 0..6 }
+  validates :day_of_week, uniqueness: { scope: :restaurant_id }
 end
