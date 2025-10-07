62b3c6ad5945b986127cccf17352430ae4f80e45
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-10-06 20:41:42 -0400

    Minot addition to cuisine serializer for fornt end

--- app/serializers/cuisine_serializer.rb
+++ app/serializers/cuisine_serializer.rb
@@ -8,6 +8,15 @@ class CuisineSerializer
         id: restaurant.id,
         name: restaurant.name,
         address: restaurant.address,
+        about: restaurant.about,
+        lunch_price: restaurant.lunch_price,
+        dinner_price: restaurant.dinner_price,
+        cuisines: restaurant.cuisines.map do |cuisine|
+          {
+            id: cuisine.id,
+            name: cuisine.name,
+          }
+        end,
         prefecture: restaurant.prefecture,
         district: restaurant.district,
         phone_number: restaurant.phone_number,
